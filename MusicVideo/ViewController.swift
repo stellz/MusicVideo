cad3348733cd52b7c20885ab456232d2c30af984
Stella Sadova <stella.sadova@tentouch.com>
2016-03-13 17:51:50 +0200

    we uncommented the first version and commented out the second approach just for reference

--- MusicVideo/ViewController.swift
+++ MusicVideo/ViewController.swift
@@ -15,17 +15,18 @@ class ViewController: UIViewController {
         
         //Call API
         let api = APIManager()
-       // api.loadData("https://itunes.apple.com/us/rss/topmusicvideos/limit=10/json", completion: didLoadData)
-        
-        api.loadData("https://itunes.apple.com/us/rss/topmusicvideos/limit=10/json") {
-            (result:String) in
-            print(result)
-        }
+        api.loadData("https://itunes.apple.com/us/rss/topmusicvideos/limit=10/json", completion: didLoadData)
+       
+        //second version without separate function
+//        api.loadData("https://itunes.apple.com/us/rss/topmusicvideos/limit=10/json") {
+//            (result:String) in
+//            print(result)
+//        }
     }
     
-//    func didLoadData(result:String) {
-//        print(result)
-//    }
+    func didLoadData(result:String) {
+        print(result)
+    }
 
 }
 
