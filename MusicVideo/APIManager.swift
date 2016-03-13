709602c4379da3fa5b82ba6339613a94c121841a
Stella Sadova <stella.sadova@tentouch.com>
2016-03-13 21:53:19 +0200

    converting the nsdata into son object

--- MusicVideo/APIManager.swift
+++ MusicVideo/APIManager.swift
@@ -24,8 +24,30 @@ class APIManager {
                     completion (result: error!.localizedDescription)
                 }
                 else {
-                    completion (result: "NSURLSession successful")
-                    print(data)
+                    //Added for JSONSerialization
+                    do {
+                    /* .AllowFragments - top level object is not Array or SDictionary
+                        Any type of string or value
+                        NSJASONSerialization requires the Do / Try / Catch
+                        Converts the NSDATA into a JSON object and cast it to a Dictionary */
+                        if let json = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments)
+                            as? [String: AnyObject] {
+                                print(json)
+                                
+                                let priority = DISPATCH_QUEUE_PRIORITY_HIGH
+                                dispatch_async(dispatch_get_global_queue(priority, 0)){
+                                    dispatch_async(dispatch_get_main_queue()){
+                                        completion (result: "JSONSerialization Successful")
+                                    }
+                                }
+                        }
+                    }
+                    catch {
+                        dispatch_async(dispatch_get_main_queue()){
+                            completion (result: "error in JSONSerialization")
+                        }
+
+                    }
                 }
             }
         }
