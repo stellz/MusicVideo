a21ca5bf87b25056c82567eea84158bf82c25523
Stella Sadova <stella.sadova@tentouch.com>
2016-03-27 21:11:07 +0300

    added detail screen for each video when you tap on one on storyboard + all constraints and preferred fonts + new class filled;

---
+++ MusicVideo/MusicVideoDetailVC.swift
@@ -0,0 +1,13 @@
+//
+//  MusicVideoDetailVC.swift
+//  MusicVideo
+//
+//  Created by Stellz on 3/27/16.
+//  Copyright © 2016 Stellz. All rights reserved.
+//
+
+import UIKit
+
+class MusicVideoDetailVC: UIViewController {
+
+}
