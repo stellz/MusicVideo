5ddb02da2fb678ecb43d489031ea97a6136f806f
Stella Sadova <stella.sadova@tentouch.com>
2016-03-28 12:30:20 +0300

    added new class file for the settings

---
+++ MusicVideo/SettingsTVC.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsTVC.swift
+//  MusicVideo
+//
+//  Created by Stella on 3/28/16.
+//  Copyright © 2016 Stellz. All rights reserved.
+//
+
+import UIKit
+
+class SettingsTVC: UITableViewController {
+
+}
